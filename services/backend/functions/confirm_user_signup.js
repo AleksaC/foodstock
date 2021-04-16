634849634873d933e370b4d1b778497f343cde64
crowfather <lukau2357@gmail.com>
2021-04-16 23:57:33 +0200

    Apparently, ES6 import issue did not get resolved by merging.

--- services/backend/functions/confirm_user_signup.js
+++ services/backend/functions/confirm_user_signup.js
@@ -1,10 +1,10 @@
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const DocumentClient = new DynamoDB.DocumentClient();
 
-module.exports.handler = async (event) => {
+export const handler = async (event) => {
     if(event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
       const usersTableName = process.env.USERS_TABLE;
-      console.log(usersTableName);
+
       const user = {
         username: event.userName,
         createdAt: new Date().toJSON()
