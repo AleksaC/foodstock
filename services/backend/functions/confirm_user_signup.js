d450908fa6ec83b3a6c82505f40307414e6e3882
Damir Delijic <delijicdamir@gmail.com>
2021-04-16 02:06:31 +0200

    Simplifying usersignup function and adding roles

--- services/backend/functions/confirm_user_signup.js
+++ services/backend/functions/confirm_user_signup.js
@@ -1,31 +1,20 @@
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const DocumentClient = new DynamoDB.DocumentClient();
-const Chance = require('chance');
-const chance = new Chance();
 
-const { userTable } = process.env;
+const { usersTableName } = process.env;
 
 module.exports.handler = async (event) => {
-	//kopirana od monaha
-    console.log("Evo me");
     const name = event.request.userAttributes['name'];
-    const suffix = chance.string({ length: 8, casing: 'upper', alpha: true, numeric: true });
-    const screenName = `${name.replace(/[^a-zA-Z0-9]/g, "")}${suffix}`;
     const user = {
       id: event.userName,
       name,
-      screenName,
-      createdAt: new Date().toJSON(),
-      followersCount: 0,
-      followingCount: 0,
-      tweetsCount: 0,
-      likesCounts: 0
+      createdAt: new Date().toJSON()
     };
 
     await DocumentClient.put({
-      TableName: userTable,
-      Item: user,
-      ConditionExpression: 'attribute_not_exists(id)'
+      TableName: usersTableName,
+      Item: user//,
+      //ConditionExpression: 'attribute_not_exists(id)'
     }).promise();
 
     return event;
