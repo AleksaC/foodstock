bf8a88d757bedcb58a46a35ced25a619a46800eb
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2021-04-20 14:30:00 +0200

    Typo fix

--- services/backend/functions/confirm-user-signup.js
+++ services/backend/functions/confirm-user-signup.js
@@ -2,7 +2,7 @@ import dynamoDB from "../libs/dynamodb";
 
 export const handler = async (event) => {
     if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
-        const usersTableName = process.env.UsersTableName;
+        const usersTableName = process.env.usersTableName;
         const user = {
             username: event.userName,
             createdAt: new Date().toJSON(),
