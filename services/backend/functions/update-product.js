c51e8aa372e833c06b1212f0ca3c0d67a0a54aeb
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2021-04-21 14:28:22 +0200

    Fixing CRUD operations and changing schema

--- services/backend/functions/update-product.js
+++ services/backend/functions/update-product.js
@@ -2,25 +2,44 @@ import handler from "../libs/handler";
 import dynamoDB from "../libs/dynamodb";
 
 export const main = handler(async (event, context) => {
-    const data = event.body;
+    const input = event.arguments.input;  // get the input
+    const inputKeys = Object.keys(input);
+    if (inputKeys.length === 1) { // only id was passed
+        throw new Error("Only the id was passed in, update doesn't make sense!");
+    }
     const tableName = process.env.productsTableName;
-
+    let updateExpression = "SET ";
+    let expressionAttributeNames = {};
+    let expressionAttributeValues = {};
+    for (let i = 0; i < inputKeys.length; i++) {
+        let key = inputKeys[i];
+        if (key === "id") {
+            // don't do anything with the id
+            continue;
+        }
+        let value = input[key];
+        let attributeName = "#" + key;
+        let attributeValue = ":" + key;
+        updateExpression += attributeName + " = " + attributeValue;
+        if (i !== inputKeys.length - 1) {
+            // add a minus to separate the attributes we want to update
+            // if it's not the last attribute
+            updateExpression += ", ";
+        }
+        expressionAttributeNames[attributeName] = key;  // the attribute
+        expressionAttributeValues[attributeValue] = value;  // the value of the attribute
+    }
     const params = {
         TableName: tableName,
         Key: {
-            id: event.pathParameters.id,
+            id: input.id, // id from the input
         },
-        UpdateExpression: "SET #briefdesc = :brief, #status = :status",
-        ExpressionAttributeNames: {
-            "#briefdesc": "briefDescription",
-            "#status": "status"
-        },
-        ExpressionAttributeValues: {
-            ":brief": data.briefDescription,
-            ":status": data.status
-        }
+        UpdateExpression: updateExpression,
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ReturnValues: "ALL_NEW"
     };
-
-    await dynamoDB.update(params);
-    return params.Key;
+    const res = await dynamoDB.update(params);
+    // console.log(res);
+    return res.Attributes;
 });
