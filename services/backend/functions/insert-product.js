3814c7b75ff5fb7ea3d2e8464f9f188ace87e25e
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2021-05-05 19:57:30 +0200

    Modified fill-db script, fixed product insert
    Modified the fill-database script so it can add both products from Voli and from Idea. Insert and update lambdas were changed, alongside the required GraphQL schema changes. Some other minor changes were implemented.

--- services/backend/functions/insert-product.js
+++ services/backend/functions/insert-product.js
@@ -1,30 +1,44 @@
 import handler from "../libs/handler";
 import dynamoDB from "../libs/dynamodb";
 import * as uuid from "uuid";
-import namespaces from "../libs/namespaces";
 import builds from "../libs/build-product-info";
 
-export const main = handler(async (event, context) => {
-    const item = event.body;
+export const main = handler(async (event) => {
+    const item = event.arguments.input;
     const tableName = process.env.productsTableName;
-    const productID = uuid.v5(item.product_id, namespaces.voliNamespace); // unique product ID
-    const params = {
-        TableName: tableName,
-        Item: {
-            id: productID,
-            name: item.name,
-            category: item.category_name,
-            briefDescription: item.brief_product_description, // every product has this
-            status: "draft",
-            nutriScore: "A",
-            images: item.image_urls,
-            description: builds.buildDescription(item),
-            nutritionalValues: builds.buildNutriValues(item),
-            currentPrice: builds.buildPrice(item),
-        },
-        // maybe add ReturnValues: "ALL_NEW"; can't have ALL_NEW, just ALL_OLD which is useless
-    };
-
-    await dynamoDB.insert(params);
-    return params.Item;
+    let insertSuccess = false;
+    while (!insertSuccess) {
+        try {
+            const productID = uuid.v4();
+            const params = {
+                TableName: tableName,
+                Item: {
+                    id: productID,
+                    name: item.name,
+                    category: item.category,
+                    briefDescription: item.briefDescription,
+                    status: item.status,
+                    nutriScore: item.nutriScore,
+                    images: item.images || [],
+                    // new
+                    store: "User created product",
+                    storeID: "0",
+                    productStoreID: "0",
+                    barcode: item.barcode || "",
+                    //
+                    description: builds.buildDescription(item.description),
+                    nutritionalValues: builds.buildNutriValues(item.nutritionalValues),
+                    currentPrice: builds.buildPrice(item.currentPrice),
+                },
+                // maybe add ReturnValues: "ALL_NEW"; can't have ALL_NEW, just ALL_OLD which is useless
+                ConditionExpression: "attribute_not_exists(id)",
+            };
+            await dynamoDB.insert(params);
+            insertSuccess = true;
+            return params.Item;
+        }
+        catch (e) {
+            console.log(e.message);
+        }
+    }
 });
